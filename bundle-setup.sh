dc20270d4b1be290f69de723c3a1945c6810c6c4
MingFei2001 <mingfei2001@gmail.com>
2023-11-18 16:25:23 +0800

    added docker.io to the list

--- bundle-setup.sh
+++ bundle-setup.sh
@@ -37,7 +37,7 @@
 # python3 python3-pip python3.10-venv lua5.1 npm
 # nodejs golang github-desktop cmake fdclone
 # libreoffice qutebrowser ranger lookatme
-# nmap pass gufw
+# nmap pass gufw docker.io
 #
 # Snap :
 # firefox neovim chromium code monophony
@@ -105,8 +105,8 @@ echo "| installing lookatme nmap gufw"
 apt install lookatme nmap gufw -y
 echo " "
 
-echo "| installing pass"
-apt install pass -y
+echo "| installing pass docker"
+apt install pass docker.io -y
 echo " "
 
 # Secondly some programming languages
