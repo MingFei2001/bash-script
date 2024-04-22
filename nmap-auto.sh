0ceebfdf0b254ae8556fe8e256d170737968c145
mingfei2001 <mingfei2001@gmail.com>
2024-04-22 09:40:59 +0800

    initiated the nmap automation script

---
+++ nmap-auto.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# TODO:Gather all kinds of scanning methods and put it all in one script
+#
+# 1. input target address
+# 2. select scan type
+# 3. start scanning
+# 4. Loop back to scan type menu
+#
+
