8b4db3dc9b42321290253d5ce3746bd40f6d2a42
mingfei <mingfei@themfminty.tail5c6886.ts.net>
2026-04-10 15:52:31 +0800

    created a new script for parallel file copy to multiple endpoint

---
+++ parallel-cp.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# This script copy file to multiple device path
+
+# List of path to devices
+USB=(MF_USB01 MF_USB02 MF_USB03 MF_USB04)
+mount_root="/media/mingfei/"
+source_file_path="$(realpath ~/Downloads/nxos64-msll.10.4.5.M.bin)"
+
+# Validate if source file exist
+if [[ ! -f "$source_file_path" ]]; then
+    echo "Error: source file not found or unset, please verify the variable in script."
+    exit 1
+fi
+
+# Loop through each device and start the transfer as process
+for device in "${USB[@]}"; do
+    target="$mount_root/$device"
+    echo "Copying $source_file_path to $target"
+    rsync -ah --progress "$source_file_path" "$target/" && echo "transfer complete" &
+done
+wait
+#
+# List the directory content to output for manual validation
+for device in "${$USB[@]}"; do
+    ls "$device "
+done
+wait
+
+echo "Running sync for finishing dirty block transfer"
+sync
+
+echo "All transfer complete."
